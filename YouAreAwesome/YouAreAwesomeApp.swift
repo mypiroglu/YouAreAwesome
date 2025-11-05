530ff4d6dce06a89deb6b8c9b5df4bcbee90dc76
Muhammet Yusuf PIROGLU <muhammetyusufpiroglu@gmail.com>
2025-11-05 13:21:47 +0300

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Muhammet Yusuf Piroğlu on 5.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
