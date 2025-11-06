53cc94f6b6d9f5162b0ba3969d87566beef609c7
Muhammet Yusuf PIROGLU <muhammetyusufpiroglu@gmail.com>
2025-11-06 11:46:33 +0300

    changed text

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-            Text("Swift is awesome!")
+            Text("I'm a developer!")
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.orange)
