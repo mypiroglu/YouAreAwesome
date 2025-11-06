f5e55a55adba9e1101b669a561daaf995010fa10
Muhammet Yusuf PIROGLU <muhammetyusufpiroglu@gmail.com>
2025-11-06 13:25:58 +0300

    added button and change text

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,17 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = "I'm a Programmer!"
+    @State var count = 0
     var body: some View {
         VStack {
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-            Text("I'm a developer!")
+            Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
-                .foregroundStyle(.orange)
-                .padding(30)
+                .foregroundStyle(.red)
+            Button("Click me!") {
+                if(count%2 == 0){
+                    message = "Awesome"
+                }
+                else{
+                    message = "I'm a Programmer!"
+                }
+                count += 1
+
+            }
             
         }
         .padding()
